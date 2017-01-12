ae7c33ce44897b3a443a971fb98111e7e940d019
Dragos <dragosdc@outlook.com>
2017-01-12 13:01:11 -0500

    drag

---
+++ app/src/main/java/com/example/dragos/tradup/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.dragos.tradup;
+
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            MobileServiceClient mClient = new MobileServiceClient(
+                    "https://tradup2.azurewebsites.net", // Replace with the Site URL
+                    this);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        MobileServiceTable<ToDoItem> mToDoTable = mClient.getTable(ToDoItem.class);
+
+    }
+
+}
